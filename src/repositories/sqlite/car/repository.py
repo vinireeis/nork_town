f77eabbc6ffb2ff2db4cbfca55ca085c4d98cdae
Vinicius Reis <v_reis@outlook.com.br>
2022-09-07 21:54:09 -0300

    separation of repositories

---
+++ src/repositories/sqlite/car/repository.py
@@ -0,0 +1,36 @@
+from src.domain.car.model import CarModel
+from src.infrastructures.sqlite.infrastructure import SqliteInfrastructure
+
+
+class CarRepository:
+
+    infra = SqliteInfrastructure
+
+    @staticmethod
+    def commit_changes_and_close_session(session):
+        session.commit()
+        session.close()
+
+    @classmethod
+    def insert_car(cls, car: CarModel):
+        session = cls.infra.get_session()
+        session.add(car)
+        cls.commit_changes_and_close_session(session=session)
+
+    @classmethod
+    def get_all_cars(cls):
+        session = cls.infra.get_session()
+        cars = session.query(CarModel).all()
+        return cars
+
+    @classmethod
+    def get_all_cars_per_customer(cls, customer_id: int):
+        session = cls.infra.get_session()
+        cars = session.query(CarModel).filter(CarModel.customer_id == customer_id)
+        return cars
+
+    @classmethod
+    def get_car_by_id(cls, car_id: int):
+        session = cls.infra.get_session()
+        car = session.query(CarModel).get(car_id)
+        return car
--- src/repositories/sqlite/customer/repository.py
+++ src/repositories/sqlite/customer/repository.py
@@ -1,62 +1,45 @@
-from src.domain.client.model import ClientModel
-from src.domain.cars.model import CarModel
+from src.domain.customer.model import CustomerModel
 from src.infrastructures.sqlite.infrastructure import SqliteInfrastructure
 
 
-class SqliteRepository:
+class CustomerRepository:
 
     infra = SqliteInfrastructure
-
-    @classmethod
-    def insert_customer(cls, client: ClientModel):
-        session = cls.infra.get_session()
-        session.add(client)
-        cls.commit_changes_and_close_session(session=session)
-
-    @classmethod
-    def insert_car(cls, car: CarModel):
-        session = cls.infra.get_session()
-        session.add(car)
-        cls.commit_changes_and_close_session(session=session)
-
-    @classmethod
-    def update_sale_opportunity_status(cls, client_id: int):
-        session = cls.infra.get_session()
-        client = session.query(ClientModel).get(client_id)
-        client.sale_opportunity = False
-        cls.commit_changes_and_close_session(session=session)
-
-    @classmethod
-    def get_all_cars_by_id(cls, customer_id: int):
-        session = cls.infra.get_session()
-        cars = session.query(CarModel).filter(CarModel.client_id == customer_id)
-        return cars
-
-    @classmethod
-    def get_customer_by_id(cls, customer_id: int):
-        session = cls.infra.get_session()
-        client = session.query(ClientModel).get(customer_id)
-        return client
-
-    @classmethod
-    def get_all_customer(cls):
-        session = cls.infra.get_session()
-        clients = session.query(ClientModel).all()
-        return clients
-
-    @classmethod
-    def get_all_cars(cls):
-        session = cls.infra.get_session()
-        clients = session.query(CarModel).all()
-        return clients
-
-    @classmethod
-    def update_one_customer(cls, customer_updated: ClientModel, customer_id: int):
-        session = cls.infra.get_session()
-        session.query(ClientModel).get(customer_id).update(customer_updated)
-        cls.commit_changes_and_close_session(session=session)
 
     @staticmethod
     def commit_changes_and_close_session(session):
         session.commit()
         session.close()
+
+    @classmethod
+    def insert_customer(cls, customer: CustomerModel):
+        session = cls.infra.get_session()
+        session.add(customer)
+        cls.commit_changes_and_close_session(session=session)
+
+    @classmethod
+    def get_all_customer(cls):
+        session = cls.infra.get_session()
+        customers = session.query(CustomerModel).all()
+        return customers
+
+    @classmethod
+    def get_customer_by_id(cls, customer_id: int):
+        session = cls.infra.get_session()
+        customer = session.query(CustomerModel).get(customer_id)
+        return customer
+
+    @classmethod
+    def update_sale_opportunity_status(cls, customer_id: int):
+        session = cls.infra.get_session()
+        customer = session.query(CustomerModel).get(customer_id)
+        customer.sale_opportunity = False
+        cls.commit_changes_and_close_session(session=session)
+
+    @classmethod
+    def update_customer_by_id(cls, customer_updated: CustomerModel, customer_id: int):
+        session = cls.infra.get_session()
+        session.query(CustomerModel).get(customer_id).update(customer_updated)
+        cls.commit_changes_and_close_session(session=session)
+
+
